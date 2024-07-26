6cd74d69ff060ce59be48315ae135108db53ffec
BreakingY <kxsun617@163.com>
2024-07-26 15:36:22 +0800

    update

--- common.cpp
+++ common.cpp
@@ -315,7 +315,7 @@ int GetSampleRateIndex(int freq){
         96000, 88200, 64000, 48000, 44100, 32000,
         24000, 22050, 16000, 12000, 11025, 8000, 7350
     };
-    for(i = 0; i< 13; i++){
+    for(i = 0; i < 13; i++){
         if(freq == freq_arr[i]){
             return i;
         }
